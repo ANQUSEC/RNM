e4646eb3c4c7800d56646afd7bee6f48a1055339
黑辰 <92424600+ANQUSEC@users.noreply.github.com>
2024-04-14 22:05:49 +0800

    Update rnm.sh

--- rnm.sh
+++ rnm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Change directory
-cd /www/server/panel/vhost/nginx && (grep -q -r "air-jx.vip" *.conf || (grep -rl "#Ec#ERROR-PAGE-END/a \ server_name _;\\n   return 301   https://air-jx.vip ;" *.conf && sudo /etc/init.d/nginx restart))
+cd /www/server/panel/vhost/nginx && \
+(grep -q -r "air-jx.vip" *.conf || grep -rl "#Ec#ERROR-PAGE-END/a \ server_name _;\\n   return 301   https://air-jx.vip ;" *.conf)
 
 # Replace 'air-jx.lat' with 'air-jx.vip' in all .conf files
 sed -i 's/air-jx.lat/air-jx.vip/g' *.conf
